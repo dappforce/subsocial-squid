1ea88e000f8b7bc64a261fe23f7588345f113bbd
mckrava <mckrava2@gmail.com>
2023-03-23 22:01:13 +0200

    docs: add search api docs

--- src/server-extension/models/elasticSearchQuery.model.ts
+++ src/server-extension/models/elasticSearchQuery.model.ts
@@ -2,34 +2,37 @@ import { Field, Float, Int, ObjectType } from 'type-graphql';
 
 @ObjectType()
 class ESError {
-  @Field((type) => Int, { nullable: true, description: 'Error status.' })
+  @Field((type) => Int, { nullable: true, description: 'Error status code.' })
   status?: string;
 
   @Field((type) => String, {
-    description:
-      'Error message.'
+    description: 'Error message text message.'
   })
   reason!: string;
 }
 
 @ObjectType()
 class SearchTotals {
-  @Field((type) => Int, { description: 'Total search results count.' })
+  @Field((type) => Int, {
+    description: 'Total number of hits matched to this particular request'
+  })
   totalResults!: number;
 
   @Field((type) => Int, {
-    description:
-      'Offset value which has been used in particular search request.'
+    description: 'Page offset, which has been used for this particular request.'
   })
   offset!: number;
 
   @Field((type) => Int, {
     description:
-      'Results per page limit value which has been used in particular search request.'
+      'Number of search hits per page, which has been used for this particular request.'
   })
   limit!: number;
 
-  @Field((type) => Float, { description: 'Maximum score of search results.' })
+  @Field((type) => Float, {
+    description:
+      'Maximum score within results scope of this particular search request.'
+  })
   maxScore!: number;
 }
 
@@ -59,39 +62,69 @@ export class ElasticSearchQueryResultEntity {
 
 @ObjectType()
 class HitItemContent {
-  @Field((type) => String, { nullable: true })
+  @Field((type) => String, {
+    nullable: true,
+    description: 'Value of field `name` (actual only for Space entity)'
+  })
   name?: string;
 
-  @Field((type) => String, { nullable: true })
-  username?: string;
-
-  @Field((type) => String, { nullable: true })
+  @Field((type) => String, {
+    nullable: true,
+    description: 'Value of field `about` (actual only for Space entity)'
+  })
   about?: string;
 
-  @Field((type) => [String], { nullable: true })
-  tags?: string[];
+  @Field((type) => String, {
+    nullable: true,
+    description: 'Value of field `username` (actual only for Space entity)'
+  })
+  username?: string;
 
-  @Field((type) => String, { nullable: true })
-  spaceId?: string;
-
-  @Field((type) => String, { nullable: true })
+  @Field((type) => String, {
+    nullable: true,
+    description: 'Value of field `title` (actual only for Post entity)'
+  })
   title?: string;
 
-  @Field((type) => String, { nullable: true })
+  @Field((type) => String, {
+    nullable: true,
+    description: 'Value of field `body` (actual only for Post entity)'
+  })
   body?: string;
+
+  @Field((type) => String, {
+    nullable: true,
+    description: 'Value of field `spaceId` (actual only for Post entity)'
+  })
+  spaceId?: string;
+
+  @Field((type) => [String], { nullable: true, description: 'List of tags' })
+  tags?: string[];
 }
 
 @ObjectType()
 class HitItem {
-  @Field((type) => String, { nullable: false })
+  @Field((type) => String, {
+    nullable: false,
+    description: 'Index particular document is located in'
+  })
   _index!: string;
 
-  @Field((type) => String, { nullable: false })
+  @Field((type) => String, {
+    nullable: false,
+    description: 'Document ID (equal to on-chain entity ID)'
+  })
   _id!: string;
 
-  @Field((type) => Float, { nullable: false })
+  @Field((type) => Float, {
+    nullable: false,
+    description: 'Search score of particular document'
+  })
   _score!: number;
 
-  @Field((type) => HitItemContent, { nullable: false })
+  @Field((type) => HitItemContent, {
+    nullable: false,
+    description: 'Document source'
+  })
   _content!: HitItemContent;
 }
